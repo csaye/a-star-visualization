19300d47846a41f42a00a2fcd5dbb79a7ea06113
Cooper Saye <coopersaye@gmail.com>
2020-08-26 00:21:19 -0700

    Implement pathfinding node.

---
+++ Assets/AStarVisualization/Scripts/Pathfinding.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AStarVisualization
+{
+    public class Pathfinding : MonoBehaviour
+    {
+        // Steps which a path can take
+        private List<Vector2Int> moveDirections = new List<Vector2Int>
+        {
+            Vector2Int.up,
+            Vector2Int.left,
+            Vector2Int.right,
+            Vector2Int.down
+        };
+
+        // The maximum number of checks before failing to find a path
+        private const int maxIterations = 1000;
+        private int iterations = 0;
+
+        // Returns shortest path from start position to end position
+        private List<Vector2Int> Path(Vector2Int start, Vector2Int end)
+        {
+            List<Vector2Int> path = new List<Vector2Int>();
+
+            return path;
+        }
+    }
+}
---
+++ Assets/AStarVisualization/Scripts/PathfindingNode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace AStarVisualization
+{
+    public class PathfindingNode : MonoBehaviour
+    {
+        public Vector2 position {get;}
+        public int steps {get;}
+        public int endDistance {get;}
+
+        public PathfindingNode(Vector2 position, int steps, int endDistance)
+        {
+            this.position = position;
+            this.steps = steps;
+            this.endDistance = endDistance;
+        }
+
+        public int totalDistance()
+        {
+            return steps + endDistance;
+        }
+    }
+}
